5369425b2dcf3020caf173758e7289886e6ed456
BogdanIlinca <bogdan.ilinca@yahoo.com>
2024-10-20 10:24:33 +0300

    Hello world push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!!</div>
+    return <div>Hello!!</div>
+    <h6>This is the idea-versus presentation site POC (proof of concept)</h6>
+    <h6>Ca sa fie accesibil de la www.idea-versus.com sau domeniul pe care l ai cumparat tu , tb sa facem modificari in
+        DNS ul celor de unde ai cumparat, ca atunci cand acceseaza domeniul tau sa fie routati sa pointeze spre adresa
+        site ului. Vedem cum se face. Rezolvam</h6>
+    <h6>Momentan este deploy at pe Netlify</h6>
+    <h6>Propunerea mea este pentru inceput sa il lasam aici si pe viitor in functie de cum evolueaza lucrurile vedem
+        unde il mutam</h6>
+    <h6>Trebuie sa vorbim si despre UI UX ...daca vrei chestii specifice legat de cum ai vrea sa arate sau ma lasi pe
+        mine initial sa fac cum cred si modificam pe parcurs </h6>
 }
