7b3d57fe8932f20ad1742d54c9d6c80325f6ee98
BogdanIlinca <bogdan.ilinca@yahoo.com>
2024-10-20 11:03:14 +0300

    Hello world push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,30 @@
 import * as React from "react"
 
 export default function Home() {
-    return <div>Hello!!</div>
-    <h6>This is the idea-versus presentation site POC (proof of concept)</h6>
-    <h6>Ca sa fie accesibil de la www.idea-versus.com sau domeniul pe care l ai cumparat tu , tb sa facem modificari in
-        DNS ul celor de unde ai cumparat, ca atunci cand acceseaza domeniul tau sa fie routati sa pointeze spre adresa
-        site ului. Vedem cum se face. Rezolvam</h6>
-    <h6>Momentan este deploy at pe Netlify</h6>
-    <h6>Propunerea mea este pentru inceput sa il lasam aici si pe viitor in functie de cum evolueaza lucrurile vedem
-        unde il mutam</h6>
-    <h6>Trebuie sa vorbim si despre UI UX ...daca vrei chestii specifice legat de cum ai vrea sa arate sau ma lasi pe
-        mine initial sa fac cum cred si modificam pe parcurs </h6>
+    return <div>
+        <img src={"/Idea Versus_Logo_RGB_Full Colour.png"} alt={"logo"} style={{ maxWidth: '60vw'}}/>
+        <h3>Hello!!</h3>
+        <h4>This is the idea-versus presentation site POC (proof of concept)</h4>
+        <h4>Am pus mai multe logo uri random din pachetul ala sa vedem cu arata</h4>
+        <h4>Ca sa fie accesibil de la www.idea-versus.com sau domeniul pe care l ai cumparat tu , tb sa facem modificari
+            in
+            DNS ul celor de unde ai cumparat, ca atunci cand acceseaza domeniul tau sa fie redirectionati sa pointeze
+            spre adresa
+            host-ului. Rezolvam</h4>
+        <h4>Momentan este deploy at pe Netlify pt ca era la indemana</h4>
+        <h4>Propunerea mea este pentru inceput sa il lasam aici si sa l facem accesibil de la www.idea-versus.com (sau cum era domeniul ala) si pe viitor in functie de cum evolueaza lucrurile vedem
+            unde il mutam</h4>
+        <h4>Trebuie sa vorbim si despre UI UX ...daca vrei chestii specifice legat de cum ai vrea sa arate sau ma lasi
+            pe
+            mine initial sa fac cum cred si modificam pe parcurs </h4>
+        <img src={"/Idea Versus_Wordmark_RGB_Light Red.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
+        <img src={"/Idea Versus_Wordmark_RGB_Black.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
+        <img src={"/Idea Versus_Wordmark_RGB_Mid Red.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
+        <img src={"/Idea Versus_Wordmark_RGB_Rich Black.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
+        <img src={"/Idea Versus_Wordmark_RGB_White.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
+        <br/>
+        <img src={"/Idea Versus_Symbol_RGB_Full Colour.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
+        <br/>
+        <img src={"/Idea Versus_Symbol_RGB_Reversed.png"} alt={"logo"} style={{ maxWidth: '5vw'}}/>
+    </div>
 }
