3dc3f3e712577d652a196bf757af017a5dc412da
BogdanIlinca <bogdan.ilinca@yahoo.com>
2024-10-20 11:07:48 +0300

    Logos and fonts included

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,18 @@ export default function Home() {
         <img src={"/Idea Versus_Logo_RGB_Full Colour.png"} alt={"logo"} style={{ maxWidth: '60vw'}}/>
         <h3>Hello!!</h3>
         <h4>This is the idea-versus presentation site POC (proof of concept)</h4>
-        <h4>Am pus mai multe logo uri random din pachetul ala sa vedem cu arata</h4>
-        <h4>Ca sa fie accesibil de la www.idea-versus.com sau domeniul pe care l ai cumparat tu , tb sa facem modificari
+        <p>Am pus mai multe logo uri random din pachetul ala sa vedem cu arata</p>
+        <p>Ca sa fie accesibil de la www.idea-versus.com sau domeniul pe care l ai cumparat tu , tb sa facem modificari
             in
             DNS ul celor de unde ai cumparat, ca atunci cand acceseaza domeniul tau sa fie redirectionati sa pointeze
             spre adresa
-            host-ului. Rezolvam</h4>
-        <h4>Momentan este deploy at pe Netlify pt ca era la indemana</h4>
-        <h4>Propunerea mea este pentru inceput sa il lasam aici si sa l facem accesibil de la www.idea-versus.com (sau cum era domeniul ala) si pe viitor in functie de cum evolueaza lucrurile vedem
-            unde il mutam</h4>
-        <h4>Trebuie sa vorbim si despre UI UX ...daca vrei chestii specifice legat de cum ai vrea sa arate sau ma lasi
+            host-ului. Rezolvam</p>
+        <p>Momentan este deploy at pe Netlify pt ca era la indemana</p>
+        <p>Propunerea mea este pentru inceput sa il lasam aici si sa l facem accesibil de la www.idea-versus.com (sau cum era domeniul ala) si pe viitor in functie de cum evolueaza lucrurile vedem
+            unde il mutam</p>
+        <p>Trebuie sa vorbim si despre UI UX ...daca vrei chestii specifice legat de cum ai vrea sa arate sau ma lasi
             pe
-            mine initial sa fac cum cred si modificam pe parcurs </h4>
+            mine initial sa fac cum cred si modificam pe parcurs </p>
         <img src={"/Idea Versus_Wordmark_RGB_Light Red.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
         <img src={"/Idea Versus_Wordmark_RGB_Black.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
         <img src={"/Idea Versus_Wordmark_RGB_Mid Red.png"} alt={"logo"} style={{ maxWidth: '10vw'}}/>
